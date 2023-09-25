2ed444d38f00a967a76b8ed5548d0ce03c12290b
Alexandre Azouri <alexandre.azouri@etu.univ-lyon1.fr>
2023-09-25 17:20:08 +0200

    Implementation Agent2

--- world.py
+++ world.py
@@ -64,10 +64,64 @@ class Agent1:
 
         if(self.obtainedOutcomes.get(self._action) is not None):
             self.anticipated_outcome = self.obtainedOutcomes[self._action]
-        self.lastAction = self._action
         self.counter += 1
+        if(self._action != self.lastAction):
+            self.counter = 0
+        self.lastAction = self._action
         return self._action
 
+class Agent2:
+    def __init__(self, valence_table):
+        """ Creating our agent """
+        self.valence_table = valence_table
+        self._action = None
+        self.anticipated_outcome = None
+        self.counter = 0
+        self.lastAction = None
+        self.obtainedOutcomes = {}
+
+    def action(self, outcome):
+        """ tracing the previous cycle """
+        if self._action is not None:
+            print("Action: " + str(self._action) +
+                  ", Anticipation: " + str(self.anticipated_outcome) +
+                  ", Outcome: " + str(outcome) +
+                  ", Satisfaction: (anticipation: " + str(self.anticipated_outcome == outcome) +
+                  ", valence: " + str(self.valence_table[self._action][outcome]) + ")")
+
+        """ Computing the next action to enact """
+        # TODO: Implement the agent's decision mechanism
+        value = 0
+        if(self.lastAction is not None):
+            value = self.valence_table[self.lastAction][outcome]
+        if(self._action is None):
+            self._action = 0
+
+        if(value < 0):
+            self._action = self.chosePosAction() if self.chosePosAction() is not None else self.lastAction
+        if(self.counter > 3):
+            self._action = (self._action +1) %2
+            self.counter = 0
+        # TODO: Implement the agent's anticipation mechanism
+        self.anticipated_outcome = 0
+        if(self.obtainedOutcomes.get(self._action) is None and self.lastAction is not None):
+            self.obtainedOutcomes[self.lastAction] = outcome
+
+        if(self.obtainedOutcomes.get(self._action) is not None):
+            self.anticipated_outcome = self.obtainedOutcomes[self._action]
+        self.counter += 1
+        if(self._action != self.lastAction):
+            self.counter = 0
+        self.lastAction = self._action
+        return self._action
+
+    def chosePosAction(self):
+        for key in self.obtainedOutcomes.keys():
+            value = self.valence_table[key][self.obtainedOutcomes[key]]
+            if(value > 0):
+                if(key != self.lastAction):
+                    return key
+        return None
 class Environment1:
     """ In Environment 1, action 0 yields outcome 0, action 1 yields outcome 1 """
     def outcome(self, action):
@@ -106,11 +160,12 @@ valences = [[-1, 1], [-1, 1]]
 # valences = [[1, -1], [1, -1]]
 # TODO Choose an agent
 #a = Agent(valences)
-a = Agent1(valences)
+#a = Agent1(valences)
+a = Agent2(valences)
 # a = Agent5(valences)
 # TODO Choose an environment
 e = Environment1()
-# e = Environment2()
+#e = Environment2()
 # e = Environment3()
 # e = TurtleSimEnacter()
 # e = TurtlePyEnacter()
@@ -120,5 +175,5 @@ if __name__ == '__main__':
     """ The main loop controlling the interaction of the agent with the environment """
     outcome = 0
     for i in range(20):
-        action = a.action(outcome)
-        outcome = e.outcome(action)
+        act = a.action(outcome)
+        outcome = e.outcome(act)
