bb2df2e58543c99dd1054f816dfe6dc5aed05b16
Alexandre Azouri <alexandre.azouri@etu.univ-lyon1.fr>
2023-09-25 16:31:48 +0200

    Implementation Agent1

--- world.py
+++ world.py
@@ -2,9 +2,9 @@
 # Olivier Georgeon, 2021.
 # This code is used to teach Developmental AI.
 # from turtlesim_enacter import TurtleSimEnacter # requires ROS
-from turtlepy_enacter import TurtlePyEnacter
+#from turtlepy_enacter import TurtlePyEnacter
 #from Agent5 import Agent5
-from OsoyooCarEnacter import OsoyooCarEnacter
+#from OsoyooCarEnacter import OsoyooCarEnacter
 ROBOT_IP = "192.168.4.1"
 
 
@@ -31,6 +31,42 @@ class Agent:
         self.anticipated_outcome = 0
         return self._action
 
+class Agent1:
+    def __init__(self, valence_table):
+        """ Creating our agent """
+        self.valence_table = valence_table
+        self._action = None
+        self.anticipated_outcome = None
+        self.counter = 0
+        self.lastAction = None
+        self.obtainedOutcomes = {}
+
+    def action(self, outcome):
+        """ tracing the previous cycle """
+        if self._action is not None:
+            print("Action: " + str(self._action) +
+                  ", Anticipation: " + str(self.anticipated_outcome) +
+                  ", Outcome: " + str(outcome) +
+                  ", Satisfaction: (anticipation: " + str(self.anticipated_outcome == outcome) +
+                  ", valence: " + str(self.valence_table[self._action][outcome]) + ")")
+
+        """ Computing the next action to enact """
+        # TODO: Implement the agent's decision mechanism
+        if(self._action is None):
+            self._action = 0
+        if(self.counter > 3):
+            self._action = (self._action +1) %2
+            self.counter = 0
+        # TODO: Implement the agent's anticipation mechanism
+        self.anticipated_outcome = 0
+        if(self.obtainedOutcomes.get(self._action) is None and self.lastAction is not None):
+            self.obtainedOutcomes[self.lastAction] = outcome
+
+        if(self.obtainedOutcomes.get(self._action) is not None):
+            self.anticipated_outcome = self.obtainedOutcomes[self._action]
+        self.lastAction = self._action
+        self.counter += 1
+        return self._action
 
 class Environment1:
     """ In Environment 1, action 0 yields outcome 0, action 1 yields outcome 1 """
@@ -69,7 +105,8 @@ class Environment3:
 valences = [[-1, 1], [-1, 1]]
 # valences = [[1, -1], [1, -1]]
 # TODO Choose an agent
-a = Agent(valences)
+#a = Agent(valences)
+a = Agent1(valences)
 # a = Agent5(valences)
 # TODO Choose an environment
 e = Environment1()
